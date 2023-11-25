cb1185117b9ab90ab183223bb98ec2599fbc043f
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-11-25 14:47:47 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
+Route::get('/appointment', function () {
+    return view('bookingconsult');
+});
