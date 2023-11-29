28498df1b3f744eb2e29a8edfaa19da98422a185
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-11-29 20:27:41 +0700

    update controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+}
+
+class ConsultationController extends Controller
+{
+    public function showForm()
+    {
+        return view('consultation.form');
+    }
+
+    // Add a method to handle form submission if needed
 }
