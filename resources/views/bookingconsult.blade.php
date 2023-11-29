24118dd5f6d37d496a3e2267fe2c0033248a1831
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-11-29 20:46:46 +0700

    update bookingconsult.blade.php

--- resources/views/bookingconsult.blade.php
+++ resources/views/bookingconsult.blade.php
@@ -7,29 +7,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking Consultation</title>
 </head>
-<!-- <body>
-
-    <h1>Book a Consultation</h1>
-
-    <form method="post" action="{{ route('consultation.submit') }}">
-        @csrf
-
-        <label for="name">Your Name:</label>
-        <input type="text" name="name" required>
-
-        <label for="email">Your Email:</label>
-        <input type="email" name="email" required>
-
-        <label for="service">Select a Service:</label>
-        <select name="service" required>
-            <option value="service_1">Service 1</option>
-            <option value="service_2">Service 2</option>
-            Add more options as needed
-        </select>
-
-        <button type="submit">Submit</button>
-    </form>
-
-</body>
-
-</html>
+<body>
+</html> 
