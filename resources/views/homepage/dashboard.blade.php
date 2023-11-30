e1c132d9dcaf5a25ceb15bd6db0ecf46d9789244
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-11-30 20:51:06 +0700

    update dashboard

--- resources/views/homepage/dashboard.blade.php
+++ resources/views/homepage/dashboard.blade.php
@@ -8,18 +8,18 @@
     <title>Consultify - Health & Care Medical</title>
 
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/favicon.ico') }}" />
 
     <!-- bootstrap.min css -->
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
     <!-- Icon Font Css -->
-    <link rel="stylesheet" href="{{ asset('plugins/icofont/icofont.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/icofont/icofont.min.css') }}">
     <!-- Slick Slider CSS -->
-    <link rel="stylesheet" href="{{ asset('plugins/slick-carousel/slick/slick.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/slick-carousel/slick/slick-theme.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/slick-carousel/slick/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/slick-carousel/slick/slick-theme.css') }}">
 
     <!-- Main Stylesheet -->
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body id="top">
