724f4d1de04aece06b7194206e479a205ca1bb1a
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-11-30 17:24:43 +0700

    update

--- resources/views/homepage/dashboard.blade.php
+++ resources/views/homepage/dashboard.blade.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
-  <meta name="author" content="themefisher.com">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="Orbitor, business, company, agency, modern, bootstrap4, tech, software">
+    <meta name="author" content="themefisher.com">
 
-  <title>Consultify - Health & Care Medical</title>
+    <title>Consultify - Health & Care Medical</title>
 
-  <!-- Favicon -->
-  <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}" />
 
-  <!-- bootstrap.min css -->
-  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
-  <!-- Icon Font Css -->
-  <link rel="stylesheet" href="plugins/icofont/icofont.min.css">
-  <!-- Slick Slider  CSS -->
-  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
-  <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.min.css') }}">
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="{{ asset('plugins/icofont/icofont.min.css') }}">
+    <!-- Slick Slider CSS -->
+    <link rel="stylesheet" href="{{ asset('plugins/slick-carousel/slick/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/slick-carousel/slick/slick-theme.css') }}">
 
-  <!-- Main Stylesheet -->
-  <link rel="stylesheet" href="css/style.css">
-
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body id="top">
@@ -442,31 +441,18 @@
 	</div>
 </footer>
    
-
-    <!-- 
-    Essential Scripts
-    =====================================-->
-
-    
-    <!-- Main jQuery -->
-    <script src="plugins/jquery/jquery.js"></script>
-    <!-- Bootstrap 4.3.2 -->
-    <script src="plugins/bootstrap/js/popper.js"></script>
-    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="plugins/counterup/jquery.easing.js"></script>
-    <!-- Slick Slider -->
-    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
-    <!-- Counterup -->
-    <script src="plugins/counterup/jquery.waypoints.min.js"></script>
-    
-    <script src="plugins/shuffle/shuffle.min.js"></script>
-    <script src="plugins/counterup/jquery.counterup.min.js"></script>
-    <!-- Google Map -->
-    <script src="plugins/google-map/map.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>    
-    
-    <script src="js/script.js"></script>
-    <script src="js/contact.js"></script>
-
-  </body>
-  </html>
+<!-- Essential Scripts -->
+<script src="{{ asset('plugins/jquery/jquery.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/popper.js') }}"></script>
+<script src="{{ asset('plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('plugins/counterup/jquery.easing.js') }}"></script>
+<script src="{{ asset('plugins/slick-carousel/slick/slick.min.js') }}"></script>
+<script src="{{ asset('plugins/counterup/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('plugins/shuffle/shuffle.min.js') }}"></script>
+<script src="{{ asset('plugins/counterup/jquery.counterup.min.js') }}"></script>
+<script src="{{ asset('plugins/google-map/map.js') }}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&callback=initMap"></script>
+<script src="{{ asset('js/script.js') }}"></script>
+<script src="{{ asset('js/contact.js') }}"></script>
+</body>
+</html>
