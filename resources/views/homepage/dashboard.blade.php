165adc0acb1cc156170ce1ef46da14a9b9a1b3a2
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-12-01 09:12:09 +0700

    update dashboard

--- resources/views/homepage/dashboard.blade.php
+++ resources/views/homepage/dashboard.blade.php
@@ -5,7 +5,7 @@
   <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
   <meta name="author" content="themefisher.com">
 
-  <title>Consultify - Health & Care Medical template</title>
+  <title>Consultify - Find your happiness here</title>
 
   <!-- Favicon -->
   <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
@@ -32,14 +32,14 @@
 				<div class="col-lg-6">
 					<ul class="top-bar-info list-inline-item pl-0 mb-0">
 						<li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>support@consultify.com</a></li>
-						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Address Ta-134/A, New York, USA </li>
+						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Bojongsoang, Bandung</li>
 					</ul>
 				</div>
 				<div class="col-lg-6">
 					<div class="text-lg-right top-right-bar mt-2 mt-lg-0">
 						<a href="tel:+23-345-67890" >
 							<span>Call Now : </span>
-							<span class="h4">823-4565-13456</span>
+							<span class="h4">021-xxx-xxx</span>
 						</a>
 					</div>
 				</div>
@@ -49,7 +49,7 @@
 	<nav class="navbar navbar-expand-lg navigation" id="navbar">
 		<div class="container">
 		 	 <a class="navbar-brand" href="home.php">
-			  <img src="assets/images/consultify.png" alt="" class="img-fluid" style="width: auto; height: 200px;">
+			  <img src="assets/images/consultify.png" alt="" class="img-fluid" style="width: 50px; height: 50px;">
 			</a>
 
 		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,34 +61,23 @@
 			  <li class="nav-item active">
 				<a class="nav-link" href="home.php">Home</a>
 			  </li>
-			   <li class="nav-item"><a class="nav-link" href="about.html">About</a></li>
-			    <li class="nav-item"><a class="nav-link" href="service.html">Services</a></li>
-
-			    <li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Department <i class="icofont-thin-down"></i></a>
-					<ul class="dropdown-menu" aria-labelledby="dropdown02">
-						<li><a class="dropdown-item" href="department.html">Departments</a></li>
-						<li><a class="dropdown-item" href="department-single.html">Department Single</a></li>
-					</ul>
-			  	</li>
 
 			  	<li class="nav-item dropdown">
-					<a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Doctors <i class="icofont-thin-down"></i></a>
+					<a class="nav-link dropdown-toggle" href="doctor.html" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Conselour <i class="icofont-thin-down"></i></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdown03">
-						<li><a class="dropdown-item" href="doctor.html">Doctors</a></li>
-						<li><a class="dropdown-item" href="doctor-single.html">Doctor Single</a></li>
-						<li><a class="dropdown-item" href="appoinment.html">Appoinment</a></li>
+						<li><a class="dropdown-item" href="conselour.html">Conselour</a></li>
+						<li><a class="dropdown-item" href="appoinment.html">Booking now</a></li>
 					</ul>
 			  	</li>
 
-			   <li class="nav-item dropdown">
+			   <!-- <li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="blog-sidebar.html" id="dropdown05" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i class="icofont-thin-down"></i></a>
 					<ul class="dropdown-menu" aria-labelledby="dropdown05">
 						<li><a class="dropdown-item" href="blog-sidebar.html">Blog with Sidebar</a></li>
 
 						<li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
 					</ul>
-			  	</li>
+			  	</li> -->
 			   <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
 			   <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
 			</ul>
@@ -280,7 +269,7 @@
 			<div class="row align-items-center justify-content-between">
 				<div class="col-lg-6">
 					<div class="copyright">
-						&copy; Copyright Reserved to <span class="text-color">Novena</span> by <a href="https://themefisher.com/" target="_blank">Themefisher</a>
+						&copy; Copyright Reserved to <span class="text-color">Consultify</span> by Kelompok 2
 					</div>
 				</div>
 				<div class="col-lg-6">
