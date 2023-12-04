c71f1d01f4b30e13fe2533d9226bdca15e2753d4
Miracle2eg <mira.eliz27@gmail.com>
2023-12-04 22:17:15 +0700

    update home.blade

--- resources/views/homepage/home.blade.php
+++ resources/views/homepage/home.blade.php
@@ -176,7 +176,7 @@
 			</div>
 			<div class="col-lg-4">
 				<div class="about-content pl-4 mt-4 mt-lg-0">
-					<h2 class="title-color">Personal care <br>& healthy living</h2>
+					<h2 class="title-color">Konsultasi <br>& healthy living</h2>
 					<p class="mt-4 mb-5">We provide best leading medicle service Nulla perferendis veniam deleniti ipsum officia dolores repellat laudantium obcaecati neque.</p>
 
 					<a href="service.html" class="btn btn-main-2 btn-round-full btn-icon">Services<i class="icofont-simple-right ml-3"></i></a>
