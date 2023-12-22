6fe82bb4f172f4a7dacde02f8b10b9a845e49682
hehermawanalghozy <121949569+hehermawanalghozy@users.noreply.github.com>
2023-12-22 10:07:12 +0700

    update

--- resources/views/header/artikel.blade.php
+++ resources/views/header/artikel.blade.php
@@ -48,7 +48,7 @@
 	</div>
 	<nav class="navbar navbar-expand-lg navigation" id="navbar">
 		<div class="container">
-		<a class="navbar-brand" href="about.php">Consultify<span>.</span></a>
+		<a class="navbar-brand" href="about.php">Artikel<span>.</span></a>
 
 		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="icofont-navigation-menu"></span>
@@ -173,13 +173,13 @@
 
 			<div class="blog-item-content">
 				<div class="blog-item-meta mb-3 mt-4">
-					<span class="text-muted text-capitalize mr-3"><i class="icofont-comment mr-2"></i>5 Comments</span>
-					<span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i> 28th January</span>
+					<span class="text-muted text-capitalize mr-3"><i class="icofont-comment mr-2"></i>5 Komentar</span>
+					<span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i> 28 January</span>
 				</div> 
 
-				<h2 class="mt-3 mb-3"><a href="blog-single.html">Choose quality service over cheap service  all type of things</a></h2>
+				<h2 class="mt-3 mb-3"><a href="blog-single.html">Mari konsultasi kesehatan setiap tahun</a></h2>
 
-				<p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis aliquid architecto facere commodi cupiditate omnis voluptatibus inventore atque velit cum rem id assumenda quam recusandae ipsam ea porro, dicta ad.</p>
+				<p class="mb-4">Kesehatan adalah pilar penting dalam kehidupan, karena untuk apa kaya dan menawan tetapi kita tidak sehat. Maka dari itu, konsultasi kesehatan merupakan alternatif untuk menjaga kesehatan secara berkala.</p>
 
 				<a href="blog-single.html" target="_blank" class="btn btn-main btn-icon btn-round-full">Read More <i class="icofont-simple-right ml-2  "></i></a>
 			</div>
@@ -194,13 +194,13 @@
 
 			<div class="blog-item-content">
 				<div class="blog-item-meta mb-3 mt-4">
-					<span class="text-muted text-capitalize mr-3"><i class="icofont-comment mr-2"></i>5 Comments</span>
-					<span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i> 28th January</span>
+					<span class="text-muted text-capitalize mr-3"><i class="icofont-comment mr-2"></i>5 Komentar</span>
+					<span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i> 28 Januari</span>
 				</div> 
 
-				<h2 class="mt-3 mb-3"><a href="blog-single.html">All test cost 25% in always in our laboratory</a></h2>
+				<h2 class="mt-3 mb-3"><a href="blog-single.html">Pentingnya memilih obat yang benar</a></h2>
 				
-				<p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis aliquid architecto facere commodi cupiditate omnis voluptatibus inventore atque velit cum rem id assumenda quam recusandae ipsam ea porro, dicta ad.</p>
+				<p class="mb-4">Zaman sekarang kita pasti sering mendengar berita palsu. Namun, sekarang tak hanya berita saja yang palsu, tetapi obat pun banyak yang palsu. Kita semua tahu bahwa berita palsu sangat berbahaya, begiru pula dengan obat palsu.</p>
 
 				<a href="blog-single.html" target="_blank" class="btn btn-main btn-icon btn-round-full">Read More <i class="icofont-simple-right ml-2  "></i></a>
 			</div>
@@ -216,12 +216,12 @@
 
 			<div class="blog-item-content">
 				<div class="blog-item-meta mb-3 mt-4">
-					<span class="text-muted text-capitalize mr-3"><i class="icofont-comment mr-2"></i>5 Comments</span>
-					<span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i> 28th January</span>
+					<span class="text-muted text-capitalize mr-3"><i class="icofont-comment mr-2"></i>5 Komentar</span>
+					<span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i> 28 January</span>
 				</div> 
-				<h2 class="mt-3 mb-3"><a href="blog-single.html">Get Free consulation from our special surgeon and doctors</a></h2>
+				<h2 class="mt-3 mb-3"><a href="blog-single.html">Tak perlu malu konsultasi</a></h2>
 
-				<p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis aliquid architecto facere commodi cupiditate omnis voluptatibus inventore atque velit cum rem id assumenda quam recusandae ipsam ea porro, dicta ad.</p>
+				<p class="mb-4">Di zaman yang sudah serba modern ini, tentunya kita bisa mendapatkan banyak informasi mudah. Salah satunya dengan melakukan konsultasi. Konsulytasi sangat penting dan bermanfaat untuk kehidupan.</p>
 
 				<a href="blog-single.html" target="_blank" class="btn btn-main btn-icon btn-round-full">Read More <i class="icofont-simple-right ml-2  "></i></a>
 			</div>
@@ -232,55 +232,55 @@
             <div class="col-lg-4">
                 <div class="sidebar-wrap pl-lg-4 mt-5 mt-lg-0">
 	<div class="sidebar-widget search  mb-3 ">
-		<h5>Search Here</h5>
+		<h5>Cari disini</h5>
 		<form action="#" class="search-form">
-			<input type="text" class="form-control" placeholder="search">
+			<input type="text" class="form-control" placeholder="Cari">
 			<i class="ti-search"></i>
 		</form>
 	</div>
 
 
 	<div class="sidebar-widget latest-post mb-3">
-		<h5>Popular Posts</h5>
+		<h5>Postingan Populer</h5>
 
         <div class="py-2">
         	<span class="text-sm text-muted">03 Mar 2018</span>
-            <h6 class="my-2"><a href="#">Thoughtful living in los Angeles</a></h6>
+            <h6 class="my-2"><a href="#">Pentingnya air yang sehat dalam kehidupan.</a></h6>
         </div>
 
         <div class="py-2">
-       		<span class="text-sm text-muted">03 Mar 2018</span>
-            <h6 class="my-2"><a href="#">Vivamus molestie gravida turpis.</a></h6>
+       		<span class="text-sm text-muted">07 Mar 2021</span>
+            <h6 class="my-2"><a href="#">Keluarga Harmonis dengan lakukan ritual ini.</a></h6>
         </div>
 
         <div class="py-2">
-        	<span class="text-sm text-muted">03 Mar 2018</span>
-            <h6 class="my-2"><a href="#">Fusce lobortis lorem at ipsum semper sagittis</a></h6>
+        	<span class="text-sm text-muted">03 Mei 2019</span>
+            <h6 class="my-2"><a href="#">Olahraga sebelum berkerja dapat meningkatkan mood.</a></h6>
         </div>
 	</div>
 
 	<div class="sidebar-widget category mb-3">
-		<h5 class="mb-4">Categories</h5>
+		<h5 class="mb-4">Kategori</h5>
 
 		<ul class="list-unstyled">
 		  <li class="align-items-center">
-		    <a href="#">Medicine</a>
+		    <a href="#">Kesehatan</a>
 		    <span>(14)</span>
 		  </li>
 		  <li class="align-items-center">
-		    <a href="#">Equipments</a>
+		    <a href="#">Habbits</a>
 		    <span>(2)</span>
 		  </li>
 		  <li class="align-items-center">
-		    <a href="#">Heart</a>
+		    <a href="#">Cinta</a>
 		    <span>(10)</span>
 		  </li>
 		  <li class="align-items-center">
-		    <a href="#">Free counselling</a>
+		    <a href="#">Konseling gratis</a>
 		    <span>(5)</span>
 		  </li>
 		  <li class="align-items-center">
-		    <a href="#">Lab test</a>
+		    <a href="#">Pekerjaan</a>
 		    <span>(5)</span>
 		  </li>
 		</ul>
@@ -290,39 +290,39 @@
 	<div class="sidebar-widget tags mb-3">
 		<h5 class="mb-4">Tags</h5>
 
-		<a href="#">Doctors</a>
-		<a href="#">agency</a>
-		<a href="#">company</a>
-		<a href="#">medicine</a>
-		<a href="#">surgery</a>
+		<a href="#">Kesehatan</a>
+		<a href="#">Pekerjaan</a>
+		<a href="#">Karir</a>
+		<a href="#">Obat</a>
+		<a href="#">Habbits</a>
 		<a href="#">Marketing</a>
-		<a href="#">Social Media</a>
+		<a href="#">Sosial Media</a>
 		<a href="#">Branding</a>
-		<a href="#">Laboratory</a>
+		<a href="#">Mental health</a>
 	</div>
 
 
 	<div class="sidebar-widget schedule-widget mb-3">
-		<h5 class="mb-4">Time Schedule</h5>
+		<h5 class="mb-4">Jam Kerja</h5>
 
 		<ul class="list-unstyled">
 		  <li class="d-flex justify-content-between align-items-center">
-		    <a href="#">Monday - Friday</a>
+		    <a href="#">Senin - Rabu</a>
+		    <span>8:00 - 17:00</span>
+		  </li>
+		  <li class="d-flex justify-content-between align-items-center">
+		    <a href="#">Kamis - Jumat</a>
 		    <span>9:00 - 17:00</span>
 		  </li>
 		  <li class="d-flex justify-content-between align-items-center">
-		    <a href="#">Saturday</a>
-		    <span>9:00 - 16:00</span>
-		  </li>
-		  <li class="d-flex justify-content-between align-items-center">
-		    <a href="#">Sunday</a>
-		    <span>Closed</span>
+		    <a href="#">Sabtu - Minggu</a>
+		    <span>10:00 - 17:00</span>
 		  </li>
 		</ul>
 
 		<div class="sidebar-contatct-info mt-4">
-			<p class="mb-0">Need Urgent Help?</p>
-			<h3>+23-4565-65768</h3>
+			<p class="mb-0">Bantuan Darurat</p>
+			<h3>1-800-700-6200</h3>
 		</div>
 	</div>
 
