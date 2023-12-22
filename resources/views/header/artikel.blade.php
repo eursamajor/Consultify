87009ef4dfcc6280cbd5454c789acc11aa3fc158
hehermawanalghozy <121949569+hehermawanalghozy@users.noreply.github.com>
2023-12-22 09:47:57 +0700

    upgrade

--- resources/views/header/artikel.blade.php
+++ resources/views/header/artikel.blade.php
@@ -25,6 +25,50 @@
 
 <body id="top">
 
+<header>
+	<div class="header-top-bar">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-6">
+					<ul class="top-bar-info list-inline-item pl-0 mb-0">
+						<li class="list-inline-item"><a href="mailto:support@gmail.com"><i class="icofont-support-faq mr-2"></i>support@consultify.com</a></li>
+						<li class="list-inline-item"><i class="icofont-location-pin mr-2"></i>Bandung, Indonesia </li>
+					</ul>
+				</div>
+				<div class="col-lg-6">
+					<div class="text-lg-right top-right-bar mt-2 mt-lg-0">
+						<a href="tel:+021-xxx-xxx" >
+							<span>Call Now : </span>
+							<span class="h4">021-xxx-xxx</span>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<nav class="navbar navbar-expand-lg navigation" id="navbar">
+		<div class="container">
+		<a class="navbar-brand" href="about.php">Consultify<span>.</span></a>
+
+		  	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="icofont-navigation-menu"></span>
+		  </button>
+	  
+		  <div class="collapse navbar-collapse" id="navbarmain">
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="about.php">Home</a>
+			  </li>
+				<li class="nav-item"><a class="nav-link" href="artikel.php">Artikel</a></li>
+				<li class="nav-item"><a class="nav-link" href="konselor.php">Konselor</a></li>
+			   <li class="nav-item"><a class="nav-link" href="riwayat.php">Riwayat</a></li>
+			   <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+			</ul>
+		  </div>
+		</div>
+	</nav>
+</header>
+
 <!-- <header>
 	<div class="header-top-bar">
 		<div class="container">
