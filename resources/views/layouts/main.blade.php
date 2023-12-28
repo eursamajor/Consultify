513b2a912ff84db18bd0f7f821997952e2787c52
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2023-12-28 21:39:31 +0700

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -60,7 +60,7 @@
 			  <li class="nav-item active">
 				<a class="nav-link" href="home.php">Home</a>
 			  </li>
-			    <li class="nav-item"><a class="nav-link" href="artikel.php">Artikel</a></li>
+			    <li class="nav-item"><a class="nav-link" href="artikel.php">Artikessss</a></li>
 				<li class="nav-item"><a class="nav-link" href="konselor.php">Konselor</a></li>
 			   <li class="nav-item"><a class="nav-link" href="riwayat.php">Riwayat</a></li>
 			   <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
