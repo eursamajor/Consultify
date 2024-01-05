36030fc603d1907dc4fde870659d386f8ce8c2be
Raditha Ariyani <astxyz@student.telkomuniversity.ac.id>
2024-01-05 17:30:52 +0700

    update

--- resources/views/booking/bookingconsult.blade.php
+++ resources/views/booking/bookingconsult.blade.php
@@ -32,10 +32,12 @@
             <div class="appoinment-wrap mt-5 mt-lg-0 pl-lg-5">
               <h2 class="mb-2 title-color">Pemesanan janji temu</h2>
               <p class="mb-4">Atur jadwal dengan konselor, pastikan agar datang 30 menit sebelum waktu yang dijadwalkan</p>
-                <form id="#" class="appoinment-form" method="post" action="#">
+                <form method="post" action="/booking">
+                  @csrf
                       <div class="row">
                           <div class="col-lg-6">
                               <div class="form-group">
+                                  <label for="jenis_konsultasi"></label>
                                   <select class="form-control" id="jenis_konsultasi">
                                     <option>Jenis Konsultasi</option>
                                     <option>Karir Pekerjaan</option>
@@ -45,6 +47,7 @@
                           </div>
                           <div class="col-lg-6">
                               <div class="form-group">
+                                <label for="konselor"></label>
                                   <select class="form-control" id="konselor">
                                     <option>Pilih Konselor</option>
                                     <option>Hermawan</option>
@@ -58,32 +61,36 @@
 
                           <div class="col-lg-6">
                               <div class="form-group">
-                                  <input name="date" id="date" type="text" class="form-control" placeholder="dd/mm/yyyy">
+                                <label for="date"></label>
+                                <input name="date" id="date" type="date" class="form-control" placeholder="dd/mm/yyyy">
                               </div>
                           </div>
 
                           <div class="col-lg-6">
                               <div class="form-group">
-                                  <input name="time" id="time" type="text" class="form-control" placeholder="Time">
+                                <label for="time"></label>
+                                <input name="time" id="time" type="time" class="form-control" placeholder="Time">
                               </div>
                           </div>
                           <div class="col-lg-6">
                               <div class="form-group">
-                                  <input name="name" id="name" type="text" class="form-control" placeholder="Full Name">
+                                <label for="name"></label>
+                                <input name="name" id="name" type="text" class="form-control" placeholder="Full Name">
                               </div>
                           </div>
 
                           <div class="col-lg-6">
                               <div class="form-group">
-                                  <input name="phone" id="phone" type="Number" class="form-control" placeholder="Phone Number">
+                                <label for="phone"></label>
+                                <input name="phone" id="phone" type="Number" class="form-control" placeholder="Phone Number">
                               </div>
                           </div>
                       </div>
                       <div class="form-group-2 mb-4">
-                          <textarea name="message" id="message" class="form-control" rows="6" placeholder="Your Message"></textarea>
+                        <label for="messahe"></label>
+                        <textarea name="message" id="message" class="form-control" rows="6" placeholder="Your Message"></textarea>
                       </div>
-
-                      <a class="btn btn-main btn-round-full" href="pembayaran.php">Lakukan Pemesanan<i class="icofont-simple-right ml-2"></i></a>
+                      <button type="submit" class="btn btn-main btn-round-full" href="pembayaran.php">Lakukan Pemesanan<i class="icofont-simple-right ml-2"></i></button>
                   </form>
               </div>
           </div>
